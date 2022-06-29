f34fb4f28fba10f1ca90be7651fd919a47a64359
Tommy Ferdian Hadimarta <trickytommy4@gmail.com>
2022-06-29 10:54:03 +0700

    adding link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,28 @@
-import { Email, GitHub, Instagram, LinkedIn } from '@mui/icons-material'
-import React from 'react'
-import "../styles/Footer.css"
+import { Email, GitHub, Instagram, LinkedIn } from "@mui/icons-material";
+import { Link } from "@mui/material";
+import React from "react";
+import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <div className='footer'>
-       
-         <div className='socialMedia'>
-            <Email/>
-            <GitHub/>
-            <LinkedIn/>
-            <Instagram/>
-         </div>
-         <p> &copy; 2022 Tommy Ferdian</p>
+    <div className="footer">
+      <div className="socialMedia">
+        <Link href="mailto:trickytommy4@gmail.com">
+        <Email />
+        </Link>
+        <Link href="https://github.com/TrickyTommy">
+        <GitHub />
+        </Link>
+        <Link href="https://www.linkedin.com/in/tommy-ferdian-hadimarta/">
+        <LinkedIn />
+        </Link>
+        <Link href="https://www.instagram.com/trickytommy">
+        <Instagram />
+        </Link>
+      </div>
+      <p> &copy; 2022 Tommy Ferdian</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
